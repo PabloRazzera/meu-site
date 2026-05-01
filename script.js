94be49ae08f57f6d5b77137062275284bbef46cb
PabloRazzera <pablohenriquerazzera123@gmail.com>
2026-05-01 16:16:33 -0300

    Remove reservation alert and add menu toggle function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js
 
-function reservar(){
-    alert("Obrigado pelo interesse na Green Garden! Entre em contato para reservar.");
+function abrirMenu(){
+    document.getElementById("menu").classList.toggle("show");
 }
